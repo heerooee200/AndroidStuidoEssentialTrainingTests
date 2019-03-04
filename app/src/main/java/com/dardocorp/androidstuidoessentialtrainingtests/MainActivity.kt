e7fef39af323907d5bedbe45d4f6ac16e1cea562
Eduardo <eduardomp@zoho.com>
2019-03-04 17:52:47 -0500

    Aprendimos a usar LOG que sirve para debuggear

--- app/src/main/java/com/dardocorp/androidstuidoessentialtrainingtests/MainActivity.kt
+++ app/src/main/java/com/dardocorp/androidstuidoessentialtrainingtests/MainActivity.kt
@@ -4,22 +4,32 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
+const val TAG="MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var strings = arrayOf("uno","tres");
-        println(strings[0]);
+        var strings = arrayOf("uno","tres")
+        println(strings[0])
 
         doSomething();
 
-        Log.d("MainActivity","onCreate");
+        Log.d(TAG,"onCreate")
 
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.d(TAG,"onStart")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG,"onResume")
     }
 
     fun doSomething(){
-        println("Hello ?");
+        println("Hello ?")
     }
 }
