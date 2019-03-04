27cfa694e58676a281f33b8acebfade8966b989c
Eduardo <eduardomp@zoho.com>
2019-03-04 17:20:47 -0500

    Agregamos metodos

--- app/src/main/java/com/dardocorp/androidstuidoessentialtrainingtests/MainActivity.kt
+++ app/src/main/java/com/dardocorp/androidstuidoessentialtrainingtests/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dardocorp.androidstuidoessentialtrainingtests
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         var strings = arrayOf("uno","tres");
         println(strings[0]);
 
+        doSomething();
 
+        Log.d("MainActivity","onCreate");
+
+    }
+
+    fun doSomething(){
+        println("Hello ?");
     }
 }
